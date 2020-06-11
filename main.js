68206accf0116c9119c860073a1697c44b5bc70f
Ramon Audain <raudain123@gmail.com>
2020-06-11 02:24:40 +0000

    Solution

--- main.js
+++ main.js
@@ -3,4 +3,16 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 
-function 
+function multiples(n){
+  let sum = 0;
+
+  for (let i = 1; i < n; i++){
+
+    if (i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+multiples(1000);
